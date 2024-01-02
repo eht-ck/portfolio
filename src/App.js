2fc50a760b3e9e2a2f1a67b0b849d03d525da41e
eht-ck <chandresh.beas@gmail.com>
2024-01-02 22:45:02 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Hero from "./components/hero";
+function App() {
+  return (<div>
+  <Hero/>
+
+  </div>);
+}
+
+export default App;
---
+++ src/components/hero.js
@@ -0,0 +1,31 @@
+import React from "react";
+import image from "../assests/image.jpg";
+
+const Hero = () => {
+  return (
+    <div class="flex flex-col md:flex-row h-screen items-center justify-center">
+      <div class="md:w-1/2 text-4xl font-bold text-center">
+        Hi, I'm <span class="text-[#915EFF]">Chandresh Kumar</span>
+        <br />
+        <span class="text-3xl">
+          B.Tech<span class="text-[#915EFF]"> NIT Jalandhar</span> IT '25
+          <br />
+        </span>
+        <span class="text-2xl">
+          I'm a full-stack developer exploring the field of cloud and devops.
+        </span>
+      </div>
+      <div class="w-1/2 text-center">
+        <figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 mx-auto">
+          <img
+            class="rounded-full w-90 h-90"
+            src={image}
+            alt="Chandresh Kumar"
+          />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
